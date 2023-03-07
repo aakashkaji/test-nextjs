2eca1bd533a92b03c1e2cb24ec0596e56efec850
aakash gupta <aakashgupta@aakashs-MacBook-Air.local>
2023-03-08 00:18:54 +0530

    first commmit

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,3 +1,18 @@
 export async function GET(request) {
   return new Response('Hello, Next.js!')
 }
+
+export const login = async() => {
+  if (req.method === 'POST') {
+    const { email, password } = req.body;
+
+    // Call your already built API with email and password parameters
+    const apiRes = await fetch('https://auth.dev-damensch.com/oAuth/Token');
+    const data = await apiRes.json();
+
+    // Return the data from the API as JSON
+    res.status(200).json(data);
+  } else {
+    res.status(405).end();
+  }
+}
